6b11a230a43e60628bc994bb4ceb8b249b99ae23
brianaung <brianaung16@gmail.com>
2024-07-28 03:36:00 +1000

    refactor: completion stop conditions + clean ups

--- lua/compl/init.lua
+++ lua/compl/init.lua
@@ -1,14 +1,6 @@
 local vim = vim
 local unpack = unpack
 
-local function au(event, callback, desc)
-	vim.api.nvim_create_autocmd(event, {
-		group = vim.api.nvim_create_augroup("Compl", { clear = false }),
-		callback = callback,
-		desc = desc,
-	})
-end
-
 local function debounce(timer, timeout, callback)
 	return function(...)
 		local argv = { ... }
@@ -17,11 +9,6 @@ local function debounce(timer, timeout, callback)
 			vim.schedule_wrap(callback)(unpack(argv))
 		end)
 	end
-end
-
-local function has_lsp_clients(bufnr)
-	local clients = vim.lsp.get_clients { bufnr = bufnr, method = "textDocument/completion" }
-	return #clients ~= 0
 end
 
 local M = {}
@@ -38,18 +25,32 @@ M.opts = {
 
 M.context = {
 	cursor = nil,
-	pending_requests = {},
 }
 
 M.completion = {
 	timer = vim.uv.new_timer(),
 	responses = {},
+	pending_requests = {},
+	-- Cancel pending completion requests
+	cancel_pending = function()
+		for _, cancel in ipairs(M.completion.pending_requests) do
+			cancel()
+		end
+		M.completion.pending_requests = {}
+	end,
 }
 
 M.info = {
 	timer = vim.uv.new_timer(),
 	bufnr = 0,
 	winids = {},
+	close_windows = function()
+		for idx, winid in ipairs(M.info.winids) do
+			if pcall(vim.api.nvim_win_close, winid, false) then
+				M.info.winids[idx] = nil
+			end
+		end
+	end,
 }
 
 function M.setup(opts)
@@ -58,11 +59,8 @@ function M.setup(opts)
 		return
 	end
 
-	-- apply user configuration options
-	opts = opts or {}
-	M.opts = vim.tbl_deep_extend("force", M.opts, opts)
-
-	-- ensure the user provides correct configs
+	-- apply and validate settings
+	M.opts = vim.tbl_deep_extend("force", M.opts, opts or {})
 	vim.validate {
 		["fuzzy"] = { M.opts.fuzzy, "b" },
 		["completion"] = { M.opts.completion, "t" },
@@ -71,128 +69,120 @@ function M.setup(opts)
 		["info.timeout"] = { M.opts.info.timeout, "n" },
 	}
 
-	_G.Completefunc = M.completefunc
+	_G.Compl = { completefunc = M.completefunc }
 
 	M.info.bufnr = vim.api.nvim_create_buf(false, true)
 	vim.api.nvim_buf_set_name(M.info.bufnr, "Compl:InfoWindow")
 	vim.fn.setbufvar(M.info.bufnr, "&buftype", "nofile")
 
-	au({ "BufEnter", "LspAttach" }, function(e)
-		vim.bo[e.buf].completefunc = "v:lua.Completefunc"
-	end, "Set completion function.")
+	local group = vim.api.nvim_create_augroup("Compl", { clear = true })
 
-	au(
-		{ "TextChangedI", "TextChangedP" },
-		debounce(M.completion.timer, M.opts.completion.timeout, M.start_completion),
-		"Trigger auto completion."
-	)
+	vim.api.nvim_create_autocmd({ "BufEnter", "LspAttach" }, {
+		group = group,
+		callback = function(args)
+			vim.bo[args.buf].completefunc = "v:lua.Compl.completefunc"
+		end,
+	})
 
-	au(
-		"CompleteChanged",
-		debounce(M.info.timer, M.opts.info.timeout, M.start_info),
-		"Show extra documentation info in a separate window."
-	)
+	vim.api.nvim_create_autocmd({ "TextChangedI", "TextChangedP" }, {
+		group = group,
+		callback = debounce(M.completion.timer, M.opts.completion.timeout, M.trigger_completion),
+	})
 
-	au(
-		"CompleteDonePre",
-		M.on_completedonepre,
-		"Additional text edits and commands to run after insert mode completion is done."
-	)
+	vim.api.nvim_create_autocmd("CompleteChanged", {
+		group = group,
+		callback = debounce(M.info.timer, M.opts.info.timeout, M.trigger_info),
+	})
 
-	au("InsertLeavePre", function()
-		M.close_info()
-	end, "Close any opened info windows when leaving insert mode.")
+	vim.api.nvim_create_autocmd("CompleteDone", {
+		group = group,
+		callback = M.on_completedone,
+	})
+
+	vim.api.nvim_create_autocmd({ "InsertLeavePre", "InsertLeave" }, {
+		group = group,
+		callback = function()
+			M.completion.timer:stop()
+			M.completion.cancel_pending()
+			M.info.timer:stop()
+			M.info.close_windows()
+		end,
+	})
 end
 
-function M.start_completion()
+function M.trigger_completion()
+	M.completion.cancel_pending()
+
 	local bufnr = vim.api.nvim_get_current_buf()
 	local winnr = vim.api.nvim_get_current_win()
 	local row, col = unpack(vim.api.nvim_win_get_cursor(winnr))
 	local line = vim.api.nvim_get_current_line()
 	local before_char = line:sub(col, col + 1)
 
-	-- Cancel pending completion requests
-	for _, cancel in ipairs(M.context.pending_requests) do
-		cancel()
+	if
+		-- No LSP clients
+		next(vim.lsp.get_clients { bufnr = bufnr, method = "textDocument/completion" }) == nil
+		-- Not a normal buffer
+		or vim.api.nvim_get_option_value("buftype", { buf = bufnr }) ~= ""
+		-- Item is selected
+		or vim.fn.complete_info()["selected"] ~= -1
+		-- Cursor is at the beginning
+		or col == 0
+		-- Char before cursor is a whitespace
+		or vim.fn.match(before_char, "\\s") ~= -1
+		-- Context didn't change
+		or vim.deep_equal(M.context.cursor, { row, col })
+	then
+		M.context.cursor = { row, col }
+		-- Do not trigger completion
+		return
 	end
-	M.context.pending_requests = {}
-
-	-- stylua: ignore start
-	-- Stop completion in these scenarios
-	-- Context didn't change
-	if vim.deep_equal(M.context.cursor, { row, col }) then return end
-	-- if it does, it needs to be updated immediately after
 	M.context.cursor = { row, col }
-
-	-- No LSP client
-	if not has_lsp_clients(bufnr) then return end
-
-	-- Not a normal buffer
-	if vim.api.nvim_get_option_value("buftype", { buf = bufnr }) ~= "" then return end
-
-	-- Not in insert mode
-	if vim.fn.mode() ~= "i" then return end
-
-	-- Halfway a mapping
-	if vim.fn.state "m" == "m" then return end
-
-	-- Item is selected
-	if vim.fn.complete_info()["selected"] ~= -1 then return end
-
-	-- Cursor is at the beginning
-	if col == 0 then return end
-
-	-- Char before cursor is a whitespace
-	if vim.fn.match(before_char, "\\s") ~= -1 then return end
-	-- stylua: ignore end
 
 	-- Make a request to get completion items
 	local position_params = vim.lsp.util.make_position_params()
-	table.insert(
-		M.context.pending_requests,
-		vim.lsp.buf_request_all(bufnr, "textDocument/completion", position_params, function(responses)
-			-- Apply itemDefaults to completion item as per the LSP specs:
-			--
-			-- "In many cases the items of an actual completion result share the same
-			-- value for properties like `commitCharacters` or the range of a text
-			-- edit. A completion list can therefore define item defaults which will
-			-- be used if a completion item itself doesn't specify the value.
-			--
-			-- If a completion list specifies a default value and a completion item
-			-- also specifies a corresponding value the one from the item is used."
-			for _, response in pairs(responses) do
-				if not response.err and response.result then
-					local items = response.result.items or response.result or {}
-					for _, item in pairs(items) do
-						local itemDefaults = response.result.itemDefaults
-						if itemDefaults then
-							-- https://github.com/neovim/neovim/blob/master/runtime/lua/vim/lsp/completion.lua#L173
-							item.insertTextFormat = item.insertTextFormat or itemDefaults.insertTextFormat
-							item.insertTextMode = item.insertTextMode or itemDefaults.insertTextMode
-							item.data = item.data or itemDefaults.data
-							if itemDefaults.editRange then
-								local textEdit = item.textEdit or {}
-								item.textEdit = textEdit
-								textEdit.newText = textEdit.newText or item.textEditText or item.insertText
-								if itemDefaults.editRange.start then
-									textEdit.range = textEdit.range or itemDefaults.editRange
-								elseif itemDefaults.editRange.insert then
-									textEdit.insert = itemDefaults.editRange.insert
-									textEdit.replace = itemDefaults.editRange.replace
-								end
+	local cancel_fn = vim.lsp.buf_request_all(bufnr, "textDocument/completion", position_params, function(responses)
+		-- Apply itemDefaults to completion item as per the LSP specs:
+		--
+		-- "In many cases the items of an actual completion result share the same
+		-- value for properties like `commitCharacters` or the range of a text
+		-- edit. A completion list can therefore define item defaults which will
+		-- be used if a completion item itself doesn't specify the value.
+		--
+		-- If a completion list specifies a default value and a completion item
+		-- also specifies a corresponding value the one from the item is used."
+		for _, response in pairs(responses) do
+			if not response.err and response.result then
+				local items = response.result.items or response.result or {}
+				for _, item in pairs(items) do
+					local itemDefaults = response.result.itemDefaults
+					if itemDefaults then
+						-- https://github.com/neovim/neovim/blob/master/runtime/lua/vim/lsp/completion.lua#L173
+						item.insertTextFormat = item.insertTextFormat or itemDefaults.insertTextFormat
+						item.insertTextMode = item.insertTextMode or itemDefaults.insertTextMode
+						item.data = item.data or itemDefaults.data
+						if itemDefaults.editRange then
+							local textEdit = item.textEdit or {}
+							item.textEdit = textEdit
+							textEdit.newText = textEdit.newText or item.textEditText or item.insertText
+							if itemDefaults.editRange.start then
+								textEdit.range = textEdit.range or itemDefaults.editRange
+							elseif itemDefaults.editRange.insert then
+								textEdit.insert = itemDefaults.editRange.insert
+								textEdit.replace = itemDefaults.editRange.replace
 							end
 						end
 					end
 				end
 			end
-			M.completion.responses = responses
+		end
+		M.completion.responses = responses
 
-			-- Trigger completefunc (add insert mode check again since this callback is async)
-			if vim.fn.mode() == "i" then
-				vim.api.nvim_feedkeys(vim.keycode "<C-x><C-u>", "m", false)
-			end
-		end)
-	)
+		if vim.fn.mode() == "i" then
+			vim.api.nvim_feedkeys(vim.keycode "<C-x><C-u>", "m", false)
+		end
+	end)
+	table.insert(M.completion.pending_requests, cancel_fn)
 end
 
 function M.completefunc(findstart, base)
@@ -344,8 +334,8 @@ function M.completefunc(findstart, base)
 	return words
 end
 
-function M.start_info()
-	M.close_info()
+function M.trigger_info()
+	M.info.close_windows()
 
 	local lsp_data = vim.tbl_get(vim.v.completed_item, "user_data", "nvim", "lsp") or {}
 	local completion_item = lsp_data.completion_item or {}
@@ -357,17 +347,17 @@ function M.start_info()
 
 	-- get resolved item only if item does not already contain documentation
 	if completion_item.documentation then
-		M.show_info(completion_item)
+		M.open_info_window(completion_item)
 	else
 		client.request("completionItem/resolve", completion_item, function(err, result)
 			if not err and result.documentation then
-				M.show_info(result)
+				M.open_info_window(result)
 			end
 		end)
 	end
 end
 
-function M.show_info(item)
+function M.open_info_window(item)
 	local detail = item.detail or ""
 
 	local documentation
@@ -428,16 +418,8 @@ function M.show_info(item)
 	end
 end
 
-function M.close_info()
-	for idx, winid in ipairs(M.info.winids) do
-		if pcall(vim.api.nvim_win_close, winid, false) then
-			M.info.winids[idx] = nil
-		end
-	end
-end
-
-function M.on_completedonepre()
-	M.close_info()
+function M.on_completedone()
+	M.info.close_windows()
 
 	local lsp_data = vim.tbl_get(vim.v.completed_item, "user_data", "nvim", "lsp") or {}
 	local completion_item = lsp_data.completion_item or {}
